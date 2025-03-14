9837188b17ca27d7fae8cdca0f1a9a21dd43c4af
mircea.talu <talumircea13@gmail.com>
2025-03-14 20:40:17 +0200

    Implement evaluate expression, together with tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use crate::ast::Expr;
 use crate::lexer::{tokenize, Token};
-use crate::parser::parse_expression;
+use crate::parser::{evaluate_expression, parse_expression};
 
 mod lexer;
 mod ast;
@@ -10,5 +10,6 @@ fn main() {
     println!("Hello, Compiler!");
     let tokenized: Vec<Token> = tokenize("5 - 2");
     let expression: Expr = parse_expression(tokenized);
-    println!("{:?}", expression);
+    let expression_result: i32 = evaluate_expression(&expression);
+    println!("{:}", expression_result);
 }
--- src/parser.rs
+++ src/parser.rs
@@ -24,9 +24,55 @@ pub fn parse_expression(tokens: Vec<Token>) -> Expr {
                     result = Expr::Subtract(Box::new(result), Box::new(Expr::Number(*value)))
                 }
             },
-            _ => panic!("Unexpected token"),
+            _ => panic!("Unexpected token")
         }
     }
 
     result
+}
+
+pub fn evaluate_expression(expr: &Expr) -> i32 {
+    match expr {
+        Expr::Number(n) => *n,
+        Expr::Add(box1, box2) => {
+            evaluate_expression(box1) + evaluate_expression(box2)
+        },
+        Expr::Subtract(box1, box2) => {
+            evaluate_expression(box1) - evaluate_expression(box2)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ast::Expr;
+    use crate::ast::Expr::{Add, Number, Subtract};
+    use crate::lexer::Token;
+    use crate::parser::{evaluate_expression, parse_expression};
+
+    #[test]
+    fn test_expression_parser() {
+        let test_cases = vec![
+            (vec![Token::NUMBER(1), Token::PLUS, Token::NUMBER(2)], Add(Box::new(Number(1)), Box::new(Number(2)))),
+            (vec![Token::NUMBER(5), Token::MINUS, Token::NUMBER(3)], Subtract(Box::new(Number(5)), Box::new(Number(3)))),
+            (vec![Token::NUMBER(42)], Number(42)),
+        ];
+        for (input, expected) in test_cases {
+            let expr: Expr = parse_expression(input);
+            assert_eq!(expr, expected);
+        }
+    }
+
+    #[test]
+    fn test_expression_evaluator() {
+        let test_cases = vec![
+            (Add(Box::new(Number(1)), Box::new(Number(2))), 3),
+            (Subtract(Box::new(Number(5)), Box::new(Number(3))), 2),
+            (Number(42), 42),
+        ];
+        for (input, expected) in test_cases {
+            let result: i32 = evaluate_expression(&input);
+            assert_eq!(result, expected);
+        }
+    }
 }
